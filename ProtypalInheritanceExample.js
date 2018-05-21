bf42fd0548784b74c10b86e911cf6336aeecf99f
Amit Saran <amit_k_saran@yahoo.com>
2018-05-21 15:10:41 -0400

    prototypal inheritance example
    example

---
+++ ProtypalInheritanceExample.js
@@ -0,0 +1,69 @@
+"use strict";
+
+//prototypal inheritance
+
+var BaseBird = {
+  getName: function(){
+    return this.state.name;
+  },
+  setName: function(name){
+    this.state.name = name;
+  },
+  getFamily: function(){
+    return this.state.family;
+  },
+  setFamily: function(family){
+    this.state.family = family;
+  }
+}
+
+var CanFly = {
+  getMaxHeight: function(){
+    return this.state.maxHeight;
+  },
+  setMaxHeight: function(maxHeight){
+    this.state.maxHeight = maxHeight;
+  },
+  sharedValue: 4
+};
+
+var CanSwim = {
+  getUnderWaterTime: function(){
+    return this.state.underWaterTime;
+  },
+  setUnderWaterTime: function(profesunderWaterTimesion){
+    this.state.underWaterTime = underWaterTime;
+  }
+}
+
+var Bird = Object.assign({}, BaseBird);
+var FlyingBird = Object.assign({}, BaseBird, CanFly);
+var FwimmingBird = Object.assign({}, BaseBird, CanSwim);
+var FlyingAndSwimmingBird = Object.assign({}, BaseBird, CanSwim, CanFly);
+var BirdFactory = {
+	create: function(birdFunctionsToBeInPrototype){
+  	var bird = Object.create(birdFunctionsToBeInPrototype);
+    //bird.state = Object.assign({});
+    return bird;
+  } 
+}
+
+var bird1 = BirdFactory.create(Bird);
+bird1.setName('bird1');
+var bird2 = BirdFactory.create(Bird);
+bird2.setName('bird2');
+
+var flyingBird1 = BirdFactory.create(FlyingBird);
+flyingBird1.setName('flyingBird1');
+var flyingBird2 = BirdFactory.create(FlyingBird);
+flyingBird2.setName('flyingBird2');
+
+var flyingAndSwimmingBird1 = BirdFactory.create(FlyingAndSwimmingBird);
+flyingAndSwimmingBird1.setName('flyingAndSwimmingBird1');
+var flyingAndSwimmingBird = BirdFactory.create(FlyingAndSwimmingBird);
+flyingAndSwimmingBird1.setName('flyingAndSwimmingBird2');
+
+console.log('End');
+
+
+
